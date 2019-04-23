5aa6381f80e98900d1c630fbd205122783a8bd9c
Jessie Yang <xy77@duke.edu>
2019-04-23 08:25:02 -0400

    create class and explore new structure

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os 
+
+import os
 import sys
 import csv # to import data
 
@@ -10,33 +11,56 @@ from io import BytesIO #to create text or binary streams from different types of
 from zipfile import ZipFile
 from xml.etree import ElementTree # 
 
+class EnergyBid:
+    #get working directory
+    dirpath = os.getcwd()
+    # get data directory
+    datadir = os.path.join(dirpath, "data")
 
 
-dirpath = os.getcwd() # get working directory
-# confirm working directory
-foldername = os.path.basename(dirpath)
-print("Directory name is : " + foldername)
+    def directory(self):
+        # confirm working directory
+        dirname = os.path.basename(self.dirpath)
+        datname = os.path.basename(self.datadir)
+        print("Directory name is : " + dirname)
+        print("XLM files are temporarily stored in: " + datname)
+        
+#%% set date and time for data query
+    def __init__(self):
+        print("Please enter the start date you want to inspect \n")
+        print("Please also follow enter the end date you want to inspect \n")
+        self.startdate = sys.argv[1]  # example startdate = 20180919
 
-#import datetime
+        self.enddate = sys.argv[2]  # example enddate = 20180929
 
-#Set Date
 
 #%% download LMP and AS files
 
-def QueryAPI(query_name):
-    api_url=f'http://oasis.caiso.com/oasisapi/SingleZip?queryname={query_name}&startdatetime=20180919T07:00-0000&enddatetime=20180920T07:00-0000&market_run_id=DAM&version=1'
-    response = requests.get(api_url)
-    zipfile = ZipFile(BytesIO(response.content))
-    output_url= dirpath
-    zipfile.extractall(output_url)
-    #Extract all members from the archive to the current working directory.
-    # Path specifies a different directory to extract to.
+    def QueryAPI(self, query_name):
+        # use fstring to replace format but why (?)
+        api_url=f'http://oasis.caiso.com/oasisapi/SingleZip?queryname={query_name}&startdatetime={self.startdate}T07:00-0000&enddatetime={self.enddate}T07:00-0000&market_run_id=DAM&version=1'
+        response = requests.get(api_url)
+        zipfile = ZipFile(BytesIO(response.content))
+        output_url= self.datadir
+        zipfile.extractall(output_url)
+        #Extract all members from the archive to the current working directory.
+        # Path specifies a different directory to extract to.
 
-QueryAPI('PRC_LMP')
-QueryAPI('PRC_AS')
-#%% parse XML files
+    #Do we let customers pick?
+    #QueryAPI('PRC_LMP')
+    #QueryAPI('PRC_AS')
+
 
  # use command line argument
 
 
+def main():
+    test = EnergyBid
+    test.directory()
+    
+    # input query name
+    test.QueryAPI()
+   
 
+ if __name__ == "__main__": main()
+   
