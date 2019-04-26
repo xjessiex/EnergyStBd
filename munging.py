09850d933255345071dd8edcddea2a333ba4f69f
Jessie Yang <xy77@duke.edu>
2019-04-26 17:38:50 -0400

    replace by original with fixed path

--- munging.py
+++ munging.py
@@ -1,27 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
+#Gets data from API
+#Make sure the file is executable if you plan to run it as a module
 
-
+import os
 import requests
 #Requests allows you to send HTTP requests without the need to manually add query strings to your URLs, or to form-encode your POST data.
 from io import BytesIO
 #A way to create text or binary streams from different types of data.
 from zipfile import ZipFile
-import os
-
-cwd = os.getcwd()
-
-#import datetime
-
-#Set Date
-
-#%%
 
 def QueryAPI(query_name):
     api_url=f'http://oasis.caiso.com/oasisapi/SingleZip?queryname={query_name}&startdatetime=20180919T07:00-0000&enddatetime=20180920T07:00-0000&market_run_id=DAM&version=1'
     response = requests.get(api_url)
     zipfile = ZipFile(BytesIO(response.content))
-    output_url="/data/"
+    output_url=os.getcwd()
     zipfile.extractall(output_url)
     #Extract all members from the archive to the current working directory.
     # Path specifies a different directory to extract to.
