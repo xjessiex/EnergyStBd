23b05183bce23ef2b5047eb171d4bcef2b335334
Jessie Yang <xy77@duke.edu>
2019-04-26 17:38:20 -0400

    build date input and check output

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ import csv # to import data
 import requests #to send HTTP requests without the need to manually add query strings to your URLs, or to form-encode your POST data.
 from io import BytesIO #to create text or binary streams from different types of data.
 from zipfile import ZipFile
-from xml.etree import ElementTree # 
+from xml.etree import ElementTree #
 
 class EnergyBid:
-    #get working directory
+    # get working directory
     dirpath = os.getcwd()
     # get data directory
     datadir = os.path.join(dirpath, "data")
 
+    def __init__(self):
+        print("Thank you for using the energy bidding optimizer!")
+        self.startdate = input("Please enter the start date you want to inspect (yyyymmdd)\n")
+        self.enddate = input("Please also enter the end date you want to inspect (yyyymmdd) \n")
 
     def directory(self):
         # confirm working directory
@@ -24,43 +28,31 @@ class EnergyBid:
         datname = os.path.basename(self.datadir)
         print("Directory name is : " + dirname)
         print("XLM files are temporarily stored in: " + datname)
-        
-#%% set date and time for data query
-    def __init__(self):
-        print("Please enter the start date you want to inspect \n")
-        print("Please also follow enter the end date you want to inspect \n")
-        self.startdate = sys.argv[1]  # example startdate = 20180919
-
-        self.enddate = sys.argv[2]  # example enddate = 20180929
 
 
 #%% download LMP and AS files
 
     def QueryAPI(self, query_name):
-        # use fstring to replace format but why (?)
+
         api_url=f'http://oasis.caiso.com/oasisapi/SingleZip?queryname={query_name}&startdatetime={self.startdate}T07:00-0000&enddatetime={self.enddate}T07:00-0000&market_run_id=DAM&version=1'
         response = requests.get(api_url)
         zipfile = ZipFile(BytesIO(response.content))
-        output_url= self.datadir
+        output_url= self.datadir # Path specifies a different directory to extract to.
         zipfile.extractall(output_url)
         #Extract all members from the archive to the current working directory.
-        # Path specifies a different directory to extract to.
 
     #Do we let customers pick?
     #QueryAPI('PRC_LMP')
     #QueryAPI('PRC_AS')
 
 
- # use command line argument
-
-
 def main():
-    test = EnergyBid
+    test = EnergyBid()
     test.directory()
-    
-    # input query name
-    test.QueryAPI()
-   
+    test.QueryAPI('PRC_LMP')
+    test.QueryAPI("PRC_AS")
 
- if __name__ == "__main__": main()
-   
+
+if __name__ == "__main__":
+    main()
+
